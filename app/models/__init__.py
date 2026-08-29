dff00b11ac8ce4aabdbe17082436bd2c71580455
Nasora-group <ndbpharm365@gmail.com>
2026-08-29 00:02:13 +0000

    feat(pos): register POS models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,48 +3,17 @@
 from .activity_log import ActivityLog
 from .base import BaseModel, TenantAwareModel, db
 from .billing import BillingPayment, Invoice, Plan, Subscription
-from .crm import (
-    Client,
-    Commercial,
-    Note,
-    Prospect,
-    Prospection,
-    Task,
-    Tour,
-    TourStop,
-    Visit,
-)
+from .crm import Client, Commercial, Note, Prospect, Prospection, Task, Tour, TourStop, Visit
 from .organization import Organization
+from .pos import CashSession, POSPayment, POSRegister, POSSale, POSSaleLine, Store
 from .role import Permission, Role, role_permission
 from .sales import Product, Sale, SaleLine, SalesTarget
 from .user import OrganizationUser, User
 
 __all__ = [
-    "db",
-    "BaseModel",
-    "TenantAwareModel",
-    "Organization",
-    "User",
-    "OrganizationUser",
-    "Role",
-    "Permission",
-    "role_permission",
-    "ActivityLog",
-    "Plan",
-    "Subscription",
-    "Invoice",
-    "BillingPayment",
-    "Commercial",
-    "Client",
-    "Prospect",
-    "Visit",
-    "Prospection",
-    "Tour",
-    "TourStop",
-    "Task",
-    "Note",
-    "Product",
-    "Sale",
-    "SaleLine",
-    "SalesTarget",
+    "db", "BaseModel", "TenantAwareModel", "Organization", "User", "OrganizationUser",
+    "Role", "Permission", "role_permission", "ActivityLog", "Plan", "Subscription",
+    "Invoice", "BillingPayment", "Commercial", "Client", "Prospect", "Visit",
+    "Prospection", "Tour", "TourStop", "Task", "Note", "Product", "Sale", "SaleLine",
+    "SalesTarget", "Store", "POSRegister", "CashSession", "POSSale", "POSSaleLine", "POSPayment",
 ]
